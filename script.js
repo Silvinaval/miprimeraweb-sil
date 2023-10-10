50ecdda5799201e1425203f2233d9f7f1da6348e
Silvinaval <valverdesilvina@gmail.com>
2023-10-10 19:31:04 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//alert("Este es mi Javascript")
+//let nombre ="Silvina";
+//nombre="Guillermina";
+
+//var nombre2 = "Laura";
+
+//const nombre3 = "Jorge";
+//nombre3="Valentin";
+
+/*console.log(nombre);
+console.log(nombre2);
+console.log(nombre3);*/
+//document.body.innerHTML = "";
+//let contenidoTitulo = "Nombre";
+
+let menu_responsive = document.querySelector(".checkbtn");
+menu_responsive.onclick = function () {
+  navBar = document.querySelector(".navbar");
+  navBar.classList.toggle("active");
+};  
+
+/*const elementByQuerySelector = document.querySelector(".fuente-clara"); 
+ console.log("querySelector()",elementByQuerySelector)
+
+/*let textoTitulo= titulo.innerHTML;
+console.log(textoTitulo);
+//titulo.innerHTML = contenidoTitulo;*/
+ 
